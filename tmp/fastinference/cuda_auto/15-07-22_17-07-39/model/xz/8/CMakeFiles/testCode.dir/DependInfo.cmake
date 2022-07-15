62123b0371ced4bb942794e22251e12746fcbd68
Leonard David <leonard.davidbereholschi@gmail.com>
2022-07-15 17:41:08 +0300

    added automatic main.cpp (4)

---
+++ tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/CMakeFiles/testCode.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/mnt/c/Users/leona/projects/tu-dortmund/fastinference/tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/model.cu" "/mnt/c/Users/leona/projects/tu-dortmund/fastinference/tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/CMakeFiles/testCode.dir/model.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "BATCH_SIZE=8"
+  "FEATURE_TYPE=int"
+  "IMPL=\"xz\""
+  "LABEL_TYPE=int"
+  "NR_LAYERS=11"
+  "OUT_PATH=\"tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8\""
+  "predict=predict_model"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/leona/projects/tu-dortmund/fastinference/tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/main.cpp" "/mnt/c/Users/leona/projects/tu-dortmund/fastinference/tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/CMakeFiles/testCode.dir/main.cpp.o"
+  "/mnt/c/Users/leona/projects/tu-dortmund/fastinference/tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/model.cpp" "/mnt/c/Users/leona/projects/tu-dortmund/fastinference/tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8/CMakeFiles/testCode.dir/model.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BATCH_SIZE=8"
+  "FEATURE_TYPE=int"
+  "IMPL=\"xz\""
+  "LABEL_TYPE=int"
+  "NR_LAYERS=11"
+  "OUT_PATH=\"tmp/fastinference/cuda_auto/15-07-22_17-07-39/model/xz/8\""
+  "predict=predict_model"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

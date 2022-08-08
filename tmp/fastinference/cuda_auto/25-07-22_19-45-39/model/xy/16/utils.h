1ba78040560274ad68b69475fcc1c53f68d19ab7
Leonard David <leonard.davidbereholschi@gmail.com>
2022-08-08 13:51:30 +0200

    deleted temp folders

--- tmp/fastinference/cuda_auto/25-07-22_19-45-39/model/xy/16/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma once 
-inline int index3D(const int x, const int y, const int z, const int sizey, const int sizez) {
-    return x*sizey*sizez + y*sizez + z;
-}
-
-#pragma once 
-inline int index4D(const int x, const int y, const int z, const int t, const int sizey, const int sizez, const int sizet){
-    return x*sizey*sizez*sizet + y*sizez*sizet + z*sizet + t;
-}
